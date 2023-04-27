f8781b4e0085d3a9c4660dff826a6ea8a87e7125
Rahul Srivastava <rahul.srivastava@gameskraft.com>
2023-04-27 16:53:06 +0530

    fix

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -39,7 +39,7 @@ const TopNav = (props)=>{
 
     return (
         <>
-            <Row justify={'space-between'} align={'middle'} style={{position:'relative',height:'5vh',width:'100%',borderBottom:'1px solid rgb(242, 245, 249)', padding:0}}>
+            <Row justify={'space-between'} align={'middle'} style={{position:'relative',height:'5vh', minHeight:43,width:'100%',borderBottom:'1px solid rgb(242, 245, 249)', padding:0}}>
                 <Col style={{marginLeft:16}}>
                     <svg width="125" height="24" viewBox="0 0 125 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M33.5872 8.77273V5.90909H44.965V8.77273H41.0275V19H37.5247V8.77273H33.5872ZM54.2226 19V5.90909H63.6572V8.77273H57.7766V11.0227H63.1714V13.8864H57.7766V16.1364H63.6317V19H54.2226ZM84.6665 5.90909V19H81.7006L76.9705 12.1222H76.8938V19H73.3398V5.90909H76.3569L81.0103 12.7614H81.1126V5.90909H84.6665ZM94.2677 19V5.90909H103.702V8.77273H97.8216V11.0227H103.217V13.8864H97.8216V16.1364H103.677V19H94.2677ZM113.027 8.77273V5.90909H124.405V8.77273H120.467V19H116.964V8.77273H113.027Z" fill="#206BC4"/>
@@ -86,7 +86,7 @@ const TopNav = (props)=>{
                 mode="horizontal" 
                 
                 items={items} 
-                style={{position:"relative", height:'5vh'}}
+                style={{position:"relative", height:'5vh', minHeight:43}}
             />
         </>
     )
